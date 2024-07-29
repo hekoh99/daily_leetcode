bff88cb24b28245516dad507f585590421d3f6a9
Haeun Ko <54932132+hekoh99@users.noreply.github.com>
2024-07-29 09:48:00 +0900

    Time: 1901 ms (20.76%), Space: 11.5 MB (63.96%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int ans = 0;
+        for(int i=0;i<rating.size() - 2;i++) {
+            for(int j=i + 1;j < rating.size() - 1;j++) {
+                if (rating[j] > rating[i]) {
+                    for(int k=j+1;k < rating.size();k++) {
+                        if (rating[k] > rating[j]) {
+                            ans++;
+                        }
+                    }
+                }
+                else if (rating[j] < rating[i]) {
+                    for(int k=j+1;k<rating.size();k++) {
+                        if (rating[k] < rating[j]) {
+                            ans++;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
