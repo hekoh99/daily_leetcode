b28c35c4b04cde6bb8c7b4a1e1ccd1baca949eb0
Haeun Ko <54932132+hekoh99@users.noreply.github.com>
2024-10-17 10:04:15 -0400

    Time: 0 ms (100.00%), Space: 7.7 MB (12.29%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string numStr = to_string(num);
+        int n = numStr.length();
+        vector<int> maxRightIndex(n);
+
+        maxRightIndex[n - 1] = n - 1;
+        for(int i=n-2;i>=0;i--) {
+            maxRightIndex[i] = (numStr[i] > numStr[maxRightIndex[i + 1]]) ? i : maxRightIndex[i + 1];
+        }
+        for(int i=0;i<n;i++) {
+            if (numStr[i] < numStr[maxRightIndex[i]]) {
+                swap(numStr[i], numStr[maxRightIndex[i]]);
+                return stoi(numStr);
+            }
+        }
+
+        return num;
+    }
+};
